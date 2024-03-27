745d6d339c6789cf9eca5558b1c9806ceadfc596
bonnie <bonnie.luo22@outlook.com>
2024-03-27 14:35:59 -0400

    change name2

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ import logging
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="http_trigger3")
-def http_trigger2(req: func.HttpRequest) -> func.HttpResponse:
+def http_trigger3(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
